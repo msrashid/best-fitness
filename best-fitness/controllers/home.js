5c03eb8ad78565a857e8295a4e384130f4d0f766
onebuntoyou <jhpdewitt@gmail.com>
2017-10-17 17:11:38 -0400

    Began router, removed html and css

---
+++ best-fitness/controllers/home.js
@@ -0,0 +1,21 @@
+const express = require('express');
+//const models = require('../models');
+
+//const passport = require('../middlewares/authentication');
+
+const Controller = {
+	registerRouter() {
+		const router = express.Router();
+
+		router.get('/', this.index);
+		router.put('/', this.create);
+	},
+	index(req,res) {
+		res.send('The secret stash.')
+	},
+	create(req, res) {
+		res.send('Good putt.')
+	},
+};
+
+module.exports = Controller.registerRouter();

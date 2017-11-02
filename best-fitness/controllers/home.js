992129747e4880b2b747020e171e3e57fb4d172f
onebuntoyou <jhpdewitt@gmail.com>
2017-11-02 13:51:01 -0400

    Redid backend router

--- best-fitness/controllers/home.js
+++ best-fitness/controllers/home.js
@@ -4,6 +4,60 @@ const passport = require('../authentication/authentication');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.redirect('/profile');
+});
+
+router.get('/profile',
+  passport.redirectIfNotLoggedIn('/register'),
+  (req, res) => {
+    res.send('The secret stash');
+});
+
+router.get('/register', (req, res) => {
+  res.render('register');
+});
+
+router.post('/register', (req, res) => {
+  models.Clients.create({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    password_hash: req.body.password,
+  }).then((user) => {
+    req.login(user, () => {
+      res.redirect('/profile');
+    })
+  })
+});
+
+router.get('/login',
+  passport.redirectIfLoggedIn('/profile'),
+  (req, res) => {
+    res.render('login')
+  }
+)
+
+router.post('/login', (req, res) => {
+  passport.authenticate('local', {
+      successRedirect: '/profile',
+      failureRedirect: '/login',
+  })(req, res);
+})
+
+router.get('/clients', (req,res) => {
+	models.Client.findAll({
+		}).then((allClients) => {
+			res.json({allClients});
+		});
+})
+
+
+
+module.exports = router;
+
+
+/*
 const Controller = {
 	registerRouter() {
 		const router = express.Router();
@@ -70,3 +124,4 @@ const Controller = {
 };
 
 module.exports = Controller.registerRouter();
+*/

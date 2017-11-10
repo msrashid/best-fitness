080944c660c1a17a16e056a49c3ff003cb1aab5d
onebuntoyou <jhpdewitt@gmail.com>
2017-11-10 13:14:53 -0500

    Created foreign key

--- best-fitness/models/client.js
+++ best-fitness/models/client.js
@@ -9,15 +9,19 @@ module.exports = (sequelize, DataTypes) => {
 		//never save plain text password
 	});
 
+	Client.associate = (models) => {
+		models.Client.belongsTo(models.User);
+	}
+
 	Client.beforeCreate((user) =>
-    new sequelize.Promise((resolve) => {
-      bcrypt.hash(user.password_hash, null, null, (err, hashedPassword) => {
-        resolve(hashedPassword);
-      });
-    }).then((hashedPw) => {
-      user.password_hash = hashedPw;
-    })
-  );
+	    new sequelize.Promise((resolve) => {
+	      bcrypt.hash(user.password_hash, null, null, (err, hashedPassword) => {
+	        resolve(hashedPassword);
+	      });
+	    }).then((hashedPw) => {
+	      user.password_hash = hashedPw;
+	    })
+    );
 
 	return Client;
 }

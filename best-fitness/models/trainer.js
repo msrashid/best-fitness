bad9938fea7ce77af8779ac78be7062da13fbad8
onebuntoyou <jhpdewitt@gmail.com>
2017-11-12 10:35:16 -0500

    Tracked new models

---
+++ best-fitness/models/trainer.js
@@ -0,0 +1,13 @@
+module.exports = (sequelize, DataTypes) => {
+
+	const Trainer = sequelize.define('Trainer', {
+		specialty: DataTypes.STRING,
+	});
+
+	Trainer.associate = (models) => {
+		models.Trainer.belongsTo(models.User);
+		models.Trainer.hasMany(models.Appointment);
+	}
+
+	return Trainer;
+}

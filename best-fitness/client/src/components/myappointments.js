5bc15578604c80ef57ee1f5e6c77cc0f7e5e2fe3
onebuntoyou <jhpdewitt@gmail.com>
2017-12-12 13:23:29 -0500

    myappointment component

---
+++ best-fitness/client/src/components/myappointments.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import './style.css'
+const moment = require('moment');
+
+class MadeAppointment extends React.Component {
+	constructor() {
+		state = {
+
+		}
+	}
+}
+
+class MyAppointments extends React.Component {
+	constructor() {
+		state = {
+			hours = [],
+		}
+	}
+}
